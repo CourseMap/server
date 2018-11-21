ab61aec6c39fbbab245fd35988303a021a1c2ec8
hugecat <hugecat@Chieh-MBPR.local>
2018-11-21 17:01:15 +0800

    add https cert

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
+const https = require('https')
 const port = 11121
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-app.listen(port, function(){
+const key = fs.readFileSync('/home/wp2018/ssl/private.key')
+const cert = fs.readFileSync('/home/wp2018/ssl/certificate.crt')
+
+var opt = {
+  key: key,
+  cert: cert
+}
+
+https.createServer(opt, app).listen(port, function(){
   console.log("---Connected---");
 })
+
+// app.listen(port, function(){
+//   console.log("---Connected---");
+// })
+
 app.use(express.static(__dirname + '/public'))
 //need this to execute "POST"
 app.use(bodyParser.json());
