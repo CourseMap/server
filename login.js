693f9f30cc324ba85cd6c1149af6c062678583aa
hugecat <hugecat@Chieh-MBPR.local>
2018-12-17 16:43:22 +0800

    add vue

--- login.js
+++ login.js
@@ -13,17 +13,17 @@ firebase.initializeApp(config);
 var account = document.getElementById("account");
 var pwd = document.getElementById("pwd");
 var registerSmtBtn = document.getElementById("regist-btn");
-registerSmtBtn.addEventListener("click", function(){
-        console.log(account.value);
-    firebase.auth().createUserWithEmailAndPassword(account.value, pwd.value).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMsg = error.message;
-    console.log(errorMsg);
-    document.getElementById("errormsg").innerHTML = errorMsg;
-  });
-},false);
 
+if(registerSmtBtn){
+  registerSmtBtn.addEventListener("click", function(){
+      firebase.auth().createUserWithEmailAndPassword(account.value, pwd.value).catch(function(error) {
+      var errorCode = error.code;
+      var errorMsg = error.message;
+      console.log(account);
+      document.getElementById("errormsg").innerHTML = errorMsg;
+    });
+  },false);
+};
 
 //login status
 var userLogin;
@@ -38,6 +38,41 @@ firebase.auth().onAuthStateChanged(function(user) {
     console.log("Bye");
   }
 });
+
+
+
+
+var depart_list = new Vue ({
+  el: '#select_dept',
+  data: {
+    departments: ["中文系 CL", "外文系 FLL", "歷史系 HIS", "台文系 TWL", "數學系 MATH", "物理系 PHYS", "化學系 CHEM", "地科系 EARS", "光電系 DPS", "機械系 ME", "化工系 CHE", "資源系 RE", "材料系 MSE", "土木系 CE", "水利系 HOE", "工科系 ES", "能源學程 IBPE", "系統系 SNME", "都計系 UP", "航太系 AA", "環工系 EV", "測量系 GM", "醫工系 BME", "會計系 ACC", "統計系 STAT", "工資系 IIM", "企管系 BA", "交管系 TCM", "護理系 NURS", "醫技系 MLSB", "醫學系 MED", "物治系 PT", "職治系 OT", "藥學系 DOPA", "法律系 LAW", "政治系 PS", "經濟系 ECON", "心理系 PSY", "電機系 EE", "資訊系 CSIE", "建築系 ARCH", "工設系 ID", "生科系 LS", "生技系 BBS"],
+    choosed_dept:[]
+},
+  methods: {
+    // deptvalue: function(){
+    //   var test = document.getElementById("dept").value;
+    //   console.log(test);
+    //   var name = document.getElementById("username").value;
+    //   console.log(name);
+    // }
+  }
+
+});
+
+
+
+// var new_user_name = document.getElementById("username");
+//
+// var new_user = firebase.auth().currentUser;
+// firebase.database().ref('user/' + new_user.uid).set({
+//   email: new_user.email,
+//   name: new_user_name.value
+//   // department:
+// }).catch(function(error) {
+//   console.log("error!");
+// });
+
+
 
 //log in
 var logEmail = document.getElementById("log__em");
@@ -55,13 +90,15 @@ if(loginBtn){
 }
 
 
+
+
 //log out
 var logoutBtn = document.getElementById("logout-btn");
 if(logoutBtn) {
   logoutBtn.addEventListener("click", function() {
     firebase.auth().signOut().then(function() {
       console.log("sign out!");
-    }, function(err) {
+    }, function(error) {
       console.log("error");
     })
   }, false);
@@ -74,7 +111,7 @@ if(verifyBtn){
   verifyBtn.addEventListener("click", function() {
     userLogin.sendEmailVerification().then(function() {
       console.log("sent!");
-    }, function(err) {
+    }, function(error) {
       console.log.error("error!");
     });
   }, false);
@@ -88,7 +125,6 @@ var chgpwdbtn
 if(document.getElementById("username").value == 0){
   document.getElementById("regist-btn").disable = true;
 }
-
 
 
 
