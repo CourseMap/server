bba219b3368a1c141646bad9c22975732896aebc
hugecat <hugecat@Chieh-MBPR.local>
2018-12-26 17:01:50 +0800

    update about page

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {
+  apiKey: "AIzaSyAuvqAuiYCipCn5NO2gSXXLvy50Mmi7jLY",
+  authDomain: "coursemap-e2f9a.firebaseapp.com",
+  databaseURL: "https://coursemap-e2f9a.firebaseio.com",
+  projectId: "coursemap-e2f9a",
+  storageBucket: "coursemap-e2f9a.appspot.com",
+  messagingSenderId: "199405435698"
+};
